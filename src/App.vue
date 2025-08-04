28d87860e0cd5d351c66cdbd61bf5b0d6f605607
xiaoming <shangcoming@gmail.com>
2025-08-04 17:14:55 +0800

    841714

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,12 @@
             <span class="iconfont icon-12345" style="margin-right: 0; color: #f87171"></span>
             <span class="sponsor" style="margin-left: 5px">赞助 </span>
           </div>
-          <div class="social-link"></div>
+
+          <div class="social-link">
+            <i class="iconfont icon-github" style="margin-left: 13px"></i>
+            <i class="iconfont icon-telegram"></i>
+            <i class="iconfont icon-tuite"></i>
+          </div>
         </div>
         <div class="right">
           <div class="right-top right-top1"><span class="iconfont icon-3dian"></span></div>
@@ -45,6 +50,37 @@
   <router-view></router-view>
 </template>
 <style scoped lang="scss">
+.social-link {
+  margin-top: 25px;
+  i.iconfont {
+    font-size: 25px;
+    margin-right: 20px;
+    display: inline-block;
+  }
+  i.icon-github {
+    transition: transform 0.3s ease;
+    font-size: 28px;
+  }
+  i.icon-github:hover {
+    cursor: pointer;
+    transform: scale(1.1);
+  }
+  i.icon-telegram {
+    transition: transform 0.3s ease;
+    color: #26a5e4;
+  }
+  i.icon-telegram:hover {
+    cursor: pointer;
+    transform: scale(1.1);
+  }
+  i.icon-tuite {
+    transition: transform 0.3s ease;
+  }
+  i.icon-tuite:hover {
+    cursor: pointer;
+    transform: scale(1.1);
+  }
+}
 .parentbox {
   position: relative;
   height: 480px;
@@ -119,25 +155,28 @@
     top: 125px;
     left: 50%;
     transform: translateX(-50%);
-    background-color: rgba(255, 255, 255, 0.6);
+    background-color: rgba(255, 255, 255, 0.5);
+    backdrop-filter: blur(5px);
     border-radius: 10px;
     transition: transform 0.1s ease;
     .main-container {
       display: flex;
       .title {
         margin: 40px 30px;
+        margin-bottom: 20px;
         font-size: 30px;
         font-family: '楷体', 'KaiTi', 'STKaiti', serif;
         font-weight: 700;
       }
       .description {
         font-size: 14px;
-        margin-bottom: 30px;
+        margin-bottom: 20px;
         margin-left: 20px;
       }
 
       ._info {
-        margin: 30px 20px;
+        margin: 20px 20px;
+        margin-bottom: 20px;
         span {
           margin: 0 20px;
         }
@@ -150,6 +189,6 @@
 }
 
 .introduc:hover {
-  transform: translateX(-50%) scale(1.05);
+  transform: translateX(-50%) scale(1.01);
 }
 </style>
